8ab0e415ec6e70b73aadfbbe4ba751d6e124fa09
topshadow <2121718893@qq.com>
2016-05-21 16:43:06 +0800

    第四个版本
    撒

--- app.js
+++ app.js
@@ -1,60 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = require('koa')();
+var logger = require('koa-logger');
+var bodyparser = require('koa-bodyparser');
+var staticCache = require('koa-static-cache');
+var errorhanlder = require('koa-errorhandler');
+var session = require('koa-generic-session');
+var MongosStore = require('koa-generic-session');
+var flash = require('koa-flash');
+var gzip = require('koa-gzip');
+var scheme = require('koa-scheme');
+var router = require('koa-router');
+var routerCache = require('koa-router-cache');
+var render = require('co-ejs');
+var config = require('config-lite');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var merge = require('merge-descriptors');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
